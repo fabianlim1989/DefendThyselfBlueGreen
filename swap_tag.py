6a1276e3c93707df2875cdca4fc86df4e310b747
Fabian Lim <fabianlim1989@gmail.com>
2016-11-12 21:34:54 +0800

    putjobsuccess to codepipeline

--- swap_tag.py
+++ swap_tag.py
@@ -1,9 +1,28 @@
 import boto3
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+code_pipeline = boto3.client('codepipeline', region_name='ap-southeast-1')
+ec2con = boto3.client('ec2', region_name='ap-southeast-1')
+elb_client = boto3.client('elb', region_name='ap-southeast-1')
+
+def put_job_success(job, message):
+    """Notify CodePipeline of a successful job
+
+    Args:
+        job: The CodePipeline job ID
+        message: A message to be logged relating to the job status
+
+    Raises:
+        Exception: Any exception thrown by .put_job_success_result()
+
+    """
+    print('Putting job success')
+    print(message)
+    code_pipeline.put_job_success_result(jobId=job)
 
 def lambda_handler(event, context):
-
-    ec2con = boto3.client('ec2', region_name='ap-southeast-1')
-    elb_client = boto3.client('elb', region_name='ap-southeast-1')
 
     reservations = ec2con.describe_instances().get('Reservations',[])
     instances = sum([[
@@ -35,7 +54,7 @@ def lambda_handler(event, context):
                     },
                 ]
             )
-            print i['InstanceId'] + ': active to inactive'
+            logger.info(i['InstanceId'] + ': active to inactive')
           elif value == 'inactive':
             instance.create_tags(
               Tags=[
@@ -53,4 +72,11 @@ def lambda_handler(event, context):
                     },
                 ]
             )
-            print i['InstanceId'] + ': inactive to active'
+            logger.info(i['InstanceId'] + ': inactive to active')
+
+            # Extract the Job ID
+            job_id = event['CodePipeline.job']['id']
+            # Extract the Job Data
+            job_data = event['CodePipeline.job']['data']
+            put_job_success(job_id, 'Updated Active')
+            return "Complete."
